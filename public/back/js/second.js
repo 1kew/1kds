738e0773e2027d67d10923f07493a29f009c44ae
一客web <web_1k@163.com>
2019-07-25 23:42:02 +0800

    二级分类--设置隐藏域进行提交

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -41,4 +41,60 @@ $(function(){
             }
         })
     }
-})
+
+    //2. 点击添加分类按钮，显示添加模态框
+    $('#addBtn').click(function(){
+        $('#addModal').modal('show');
+
+        $.ajax({
+            type: 'get',
+            url: '/category/queryTopCategoryPaging',
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            dataType: 'json',
+            success: function( info ){
+                console.log(info);
+                var htmlStr = template('stpl', info);
+                $('.dropdown-menu').html( htmlStr );
+                
+            }
+        })
+    })
+
+
+
+    // 3. 通过事件委托 给dropdown-menu下所有a标签绑定点击事件
+    $('.dropdown-menu').on('click', 'a', function(){
+        //获取a的文本
+        var txt = $(this).text();
+        //设置给dropdownText
+        $('#dropdownText').text(txt);
+
+        //获取选中id
+        var id = $(this).data('id');
+        //设置给input框
+        $('[name="categoryId"]').val( id );
+    })
+
+
+
+    // 4. 进行文件上传初始化
+        $('#fileupload').fileupload({
+            dataType: 'json',
+            done: function( e, data ){
+                console.log(data.result.picAddr );
+                var imgurl = data.result.picAddr;
+                //赋值给img
+                $('#imgBox img').attr("src", imgurl);
+
+                //将图片地址设置给input
+                $('[name="brandLogo"]').val( imgurl );
+                
+            }
+        })
+
+
+
+});
