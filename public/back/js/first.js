256cb902eef5944bd78c3c6379c6c72be6a9d51a
一客web <web_1k@163.com>
2019-07-25 21:10:56 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -9,7 +9,7 @@ $(function(){
     function render() {
         $.ajax({
             type: 'get',
-            url: ' /category/queryTopCategoryPaging',
+            url: '/category/queryTopCategoryPaging',
             data: {
                 page: currentPage,
                 pageSize: pageSize
@@ -31,7 +31,7 @@ $(function(){
                     //注册按钮点击事件
                     onPageClicked: function(a,b,c,page){
                         // 更新当前页
-                        currentPage = page;
+                        currentPage = page
                         //重新渲染
                         render();
                     }
@@ -42,6 +42,60 @@ $(function(){
     }
 
 
+    // 点击添加分类按钮，显示模态框
+    $('#addBtn').click(function(){
+        $('#addModal').modal('show');
+    })
 
 
+    // 表单校验
+    $('#form').bootstrapValidator({
+        //配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok', //校验成功
+            invalid: 'glyphicon glyphicon-remove', //校验失败
+            validating: 'glyphicon glyphicon-refresh' //校验中
+        },
+        //配置字段
+        fields: {
+            categoryName: {
+                validators: {
+                    notEmpty:{
+                        message: '一级分类不能为空'
+                    }
+                }
+            }
+        }
+    });
+
+
+
+
+    // 注册表单校验成功事件  阻止默认提交  通过ajax提交
+    $('#form').on('success.form.bv', function( e ) {
+        e.preventDefault();
+
+        //通过ajax提交
+
+        $.ajax({
+            type: 'POST',
+            url: '/category/addTopCategory',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function( info ){
+                if( info.success ) {
+                    //添加成功
+                    //关闭模态框
+                    $('#addModal').modal('hide');
+
+                    //页面重新渲染
+                    currentPage = 1;
+                    render();
+
+                    //重置模态框
+                    $('#form').data('bootstrapValidator').resetForm(true);
+                }
+            }
+        })
+    })
 });
